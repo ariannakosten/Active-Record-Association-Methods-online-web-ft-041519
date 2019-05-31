1350e0f55203c2ed0dbebd78c8bd357e816d6d30
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-31 08:04:22 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artist.name
+    self.artist.collect do |artist|
+      artist.name
   end
 end
